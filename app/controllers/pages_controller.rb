da1bc73aa25678e8707d350dd0e63e591120416a
Rohit Kumar <itsrksoni@gmail.com>
2022-11-15 05:29:23 +0000

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+
+    def home
+        # landing page and html code available under views-pages
+    end
+
+    def about
+        # about page and html code available under views pages
+    end
+
+end
